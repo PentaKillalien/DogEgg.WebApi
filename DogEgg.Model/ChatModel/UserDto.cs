a3bce7bb3442d82ddaba7e122abca1fc6c755fc9
pengzhengliang <pengzhengliang@cptgroup.cn>
2022-02-11 11:40:09 +0800

    测试SqlSugar分表

--- DogEgg.Model/ChatModel/UserDto.cs
+++ DogEgg.Model/ChatModel/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using SqlSugar;
+﻿using Microsoft.Data.Sqlite;
+using SqlSugar;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,18 +8,22 @@ using System.Threading.Tasks;
 
 namespace DogEgg.Model.ChatModel
 {
-    [SugarTable("UserDto")]
+    [SplitTable(SplitType.Day)]
+    [SugarTable("UserDto_{year}{month}{day}")]
     public class UserDto
     {
         /// <summary>
         /// Id
         /// </summary>
-        [SugarColumn(IsPrimaryKey = true, IsIdentity = true)]//主键并且自增 （string不能设置自增）
-        public int id { get; set; }
+        [SugarColumn(IsPrimaryKey = true)]//主键并且自增 （string不能设置自增）
+        public long Id { get; set; }
         public string UserName { get; set; }
 
         public string Password { get; set; }
 
         public string Email { get; set; }
+        [SplitField] //分表字段 在插入的时候会根据这个字段插入哪个表，在更新删除的时候用这个字段找出相关表
+        public DateTime CreateTime { get; set; }
     }
+
 }
--- DogEgg.UnitTest/UnitTest1.cs
+++ DogEgg.UnitTest/UnitTest1.cs
@@ -1,4 +1,7 @@
+using DogEgg.DbAccess.DogEggDbAccess;
+using DogEgg.Model.ChatModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SqlSugar;
 using System;
 using System.Collections.Generic;
 
@@ -10,13 +13,25 @@ namespace DogEgg.UnitTest
         [TestMethod]
         public void LeetCode()
         {
-            int a = -423423;
-            string b = a.ToString();
-            char[] c = b.ToCharArray();
-            Array.Reverse(c);
-            var d = Int32.Parse(c);
-            
-            System.Console.WriteLine(d);
+            var db = new SqlSugarClient(new ConnectionConfig()
+            {
+                DbType = SqlSugar.DbType.PostgreSQL,
+                InitKeyType = InitKeyType.Attribute,
+                IsAutoCloseConnection = true,
+                ConnectionString = "PORT=5432;DATABASE=LoveChat;HOST=139.224.244.190;PASSWORD=12345678;USER ID=postgres",
+                MoreSettings = new ConnMoreSettings()
+                {
+                    PgSqlIsAutoToLower = false //数据库存在大写字段的 ，需要把这个设为false
+                }
+            });
+            db.CodeFirst.SplitTables().InitTables<UserDto>();
+            db.Insertable(new UserDto()
+            {
+                Email = "21312@qq.com",
+                CreateTime = DateTime.Now,
+                UserName = "JSAOd",
+                Password = "ssss"
+            }).SplitTable().ExecuteReturnSnowflakeId();//插入并返回雪花ID
         }
     }
 }
