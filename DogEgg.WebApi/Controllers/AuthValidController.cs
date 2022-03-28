75e2411479ff26f047cea8aeb2d36386d59e59a1
pengzhengliang <pengzhengliang@cptgroup.cn>
2022-03-28 09:54:32 +0800

    1

---
+++ DogEgg.WebApi/Controllers/AuthValidController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogEgg.WebApi.Controllers
+{
+    public class AuthValidController : ControllerBase
+    {
+        /// <summary>
+        /// 权限验证
+        /// </summary>
+        /// <returns></returns>
+        [Route("/api/auth/permission/routes")]
+        [HttpPost]
+        public string AuthPermission(Object obj)
+        {
+            try
+            {
+                Console.WriteLine();
+                return "";
+
+            }
+            catch (Exception ex)
+            {
+                return $"error:{ex.Message}";
+
+            }
+
+
+        }
+    }
+}
